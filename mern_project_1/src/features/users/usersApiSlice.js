70da49da9cac81a898d9d6e833ad72125375836a
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-01-30 14:29:51 -0600

    autosave script

--- mern_project_1/src/features/users/usersApiSlice.js
+++ mern_project_1/src/features/users/usersApiSlice.js
@@ -16,15 +16,17 @@ const initialState = usersAdapter.getInitialState();
 // The keepUnusedDataFor is the number of seconds that the data will be kept in the cache
 // The transformResponse is a function that returns the usersAdapter with the initialState and the loadedUsers
 // The providesTags is a function that returns the result ids if the result is not undefined
-
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
+        // The getUsers endpoint is a query that returns the users
         getUsers: builder.query({
             query: () => '/users',
             validateStatus: (response, result) => {
                 return response.status === 200 && !!result.isError
             },
+            //keepUnusedDataFor: 5 is the number of seconds that the data will be kept in the cache
             keepUnusedDataFor: 5,
+            //transformResponse is a function that returns the usersAdapter with the initialState and the loadedUsers
             transformResponse: responseData => {
                 const loadedUsers = responseData.users.map(user => {
                     user.id = user._id
@@ -32,6 +34,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 });
                 return usersAdapter.setAll(initialState, loadedUsers);
             },
+            //providesTags is a function that returns the result ids if the result is not undefined
             providesTags: (result, error, arg) => {
                 if (result?.ids){
                        return [
@@ -43,3 +46,26 @@ export const usersApiSlice = apiSlice.injectEndpoints({
         }),
     }),
 })
+// this
+export const {
+    useGetUsersQuery,
+} = usersApiSlice;
+
+// selectUsersResult is a selector that returns the usersApiSlice endpoints getUsers select
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
+
+// memoized selector
+// selectUsersData is a selector that returns the usersApiSlice endpoints getUsers select data
+const selectUsersData = createSelector(
+    selectUsersResult,
+    usersResult => usersResult.data // usersResult.data is the data returned from the server
+)
+
+// getSelectors creates selectors for the usersAdapter, the selectors are: selectAll, selectById and selectIds
+// We are just adding in User to the selectors "selectAllUsers", "selectUserById", "selectUserIds"
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+    selectIds: selectUserIds,
+    // Pass in a selector that returns the users slice of state
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState);
