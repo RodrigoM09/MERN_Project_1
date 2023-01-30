fb3bffc2abb20fa881b06851ad92a9c9f4246e17
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-01-30 11:33:06 -0600

    autosave script

--- mern_project_1/src/App.js
+++ mern_project_1/src/App.js
@@ -1,11 +1,15 @@
 import {Routes, Route} from 'react-router-dom';
 import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from "./components/Login";
 
 
 function App() {
   return (
     <Routes>
         <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Public />} />
+            <Route path="/login" element={<Login />} />
 
         </Route>
     </Routes>
