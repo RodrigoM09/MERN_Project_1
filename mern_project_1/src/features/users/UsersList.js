7047f910657966cd9a13ae34b65361d9aafce23c
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-01-30 15:16:21 -0600

    autosave script

--- mern_project_1/src/features/users/UsersList.js
+++ mern_project_1/src/features/users/UsersList.js
@@ -1,4 +1,29 @@
+import { useGetUsersQuery } from "./usersApiSlice";
+
+// UsersList is a component that returns the usersApiSlice endpoints getUsers select data
+// The data is the users, the isLoading is the loading state, the isSuccess is the success state,
+// the isError is the error state and the error is the error message. The component returns the
+// users list if the loading state is false and the success state is true, otherwise it returns
+// the error message if the error state is true, otherwise it returns the loading message.
 const UsersList = () => {
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetUsersQuery()
+
+    let content
+
+    // if the loading state is false and the success state is true
+    if(isLoading) content = <p>Loading...</p>
+
+    // if the error state is true
+    if(isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
     return (
         <h1>UsersList</h1>
     )
